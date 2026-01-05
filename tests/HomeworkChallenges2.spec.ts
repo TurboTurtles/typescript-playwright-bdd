87c0e21d2b235fb0c1c2deec806cf6d0b7bfa9ca
TurboTurtles <mihran1990@hotmail.com>
2026-01-05 00:21:17 -0800

    chore: jan5

--- tests/HomeworkChallenges2.spec.ts
+++ tests/HomeworkChallenges2.spec.ts
@@ -21,8 +21,33 @@ test('sumNaturalEvenNumbers', () => {
 // Reverse integer
 
 test('reverseInteger', async () =>{
- function reverseInteger = (num: number): number =>{
- let reversed = "";
- for (let i = num.length - 1; i >= 0; i--)
+ const reverseInteger = (num: number): number => {
+    let sign = num < 0 ? -1 : 1;
+    let reversed = 0;
+    num = Math.abs(num);
+
+ while (num > 0){
+    let lastDigit = num % 10;
+    reversed = reversed * 10 + lastDigit;
+    num = Math.floor(num / 10);
  }
-})
+   return reversed * sign;
+}
+ 
+  console.log(reverseInteger(123)); 
+  console.log(reverseInteger(-456));
+  console.log(reverseInteger(0));
+ 
+});
+
+
+test('Check if prime, () => {
+    // Check if a number is prime (greater than 1, no divisors other than 1 and itself)
+    const isPrime = (n: number): boolean => {
+    };
+    expect(isPrime(7)).toBe(true);
+    expect(isPrime(1)).toBe(false);
+    expect(isPrime(2)).toBe(true);
+    expect(isPrime(4)).toBe(false);
+});
+
